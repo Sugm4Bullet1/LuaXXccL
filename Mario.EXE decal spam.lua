839ab4971eacaef94ec5f7d97c1724f191d23854
Sugm4Bullet1 <g00byprohax1alt@gmail.com>
2025-05-07 20:53:46 +0200

    Add files via upload

---
+++ Mario.EXE decal spam.lua
@@ -0,0 +1,15 @@
+﻿function decalspam(id)
+    for i,v in pairs(game.Workspace:GetChildren()) do
+        if v:IsA("Part") then
+            for i=0, 5 do
+                local d = Instance.new("Decal")
+                d.Name = "SpamDecal"
+                d.Face = i
+                d.Parent = v
+                d.Texture = "http://www.roblox.com/asset/?id=15088936211"
+            end
+        end
+    end
+end
+
+decalspam(15088936211)
